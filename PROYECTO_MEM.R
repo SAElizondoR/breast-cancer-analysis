e749d97b2cdecd580a91eab0ec171deb9e0395c1
Sergio <sergio.elizondord@uanl.edu.mx>
2024-07-15 20:28:58 -0600

    Añadir estadísticas descriptivas

--- PROYECTO_MEM.R
+++ PROYECTO_MEM.R
@@ -7,14 +7,11 @@ library(mlbench)
 # Cargar el conjunto de datos de cáncer de mama
 data("BreastCancer")
 
-# Estructura de los datos
+# Mostrar la estructura de los datos
 str(BreastCancer)
 
 # Mostrar los primeros 10 elementos
 head(BreastCancer, 10)
 
-# Cantidad de observaciones
-nrow(BreastCancer)
-
-# Cantidad de variables
-ncol(BreastCancer)
+# Estadísticas descriptivas
+summary(BreastCancer)
