3ee0b309e75590a96142eb0bf7551fd193233499
Sergio <sergio.elizondord@uanl.edu.mx>
2024-07-15 20:54:16 -0600

    Transformar los datos

--- PROYECTO_MEM.R
+++ PROYECTO_MEM.R
@@ -1,17 +1,25 @@
-# Cargar el paquete mlbench
 if (!requireNamespace("mlbench")) {
   install.packages("mlbench")
 }
-library(mlbench)
+library(mlbench) # Contiene el conjunto de datos a utilizar
+library(dplyr) # Biblioteca para manipulación de datos
 
 # Cargar el conjunto de datos de cáncer de mama
 data("BreastCancer")
 
+# Asignar el tipo adecuado a las columnas numéricas y
+# codificar la clase como ceros y unos
+datos <- BreastCancer %>%
+  mutate(
+    Class = ifelse(Class == "benign", 0, 1),
+    across(-Class, as.numeric)
+  )
+
 # Mostrar la estructura de los datos
-str(BreastCancer)
+str(datos)
 
 # Mostrar los primeros 10 elementos
-head(BreastCancer, 10)
+head(datos, 10)
 
 # Estadísticas descriptivas
-summary(BreastCancer)
+summary(datos)
